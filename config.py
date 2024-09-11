f0cb010464e92e31888cf85add7d25984e19cefd
Ula_Andrz <ulandrzejczak@gmail.com>
2024-09-11 10:44:24 +0200

    README change

--- config.py
+++ config.py
@@ -13,7 +13,6 @@ class Config:
 class TestConfig:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-    # Use a temporary directory for testing
     TEXT_FILE_DIR = os.path.join(BASE_DIR, "test_saved_files")
 
     if not os.path.exists(TEXT_FILE_DIR):
